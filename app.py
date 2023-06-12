8da6b1163f039e1bc411afe0a540c952550acb1d
Dachi davituri <davituridachi33@gmail.com>
2023-06-12 17:14:52 +0400

    create wsgi and requirement libraries

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
